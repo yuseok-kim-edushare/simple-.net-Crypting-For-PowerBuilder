c715341e32ec54e257c4fbc93487f8b982f91303
김유석(Yu Seok Kim) <yuseok.kim@vilac.co.kr>
2025-01-22 14:22:30 +0900

    🐛 Resolve #19 now DLL for SQL's Test work well

--- net481SQL-server/test.cs
+++ net481SQL-server/test.cs
@@ -1,7 +1,8 @@
 using NUnit.Framework;
 using System.Data.SqlTypes;
+using SecureLibrary.SQL;
 
-namespace SecureLibrary.SQL.Tests
+namespace SecureLibrary.Tests 
 {
     [TestFixture]
     public class SqlCLRCryptingTests
@@ -9,7 +10,7 @@ namespace SecureLibrary.SQL.Tests
         private SqlString plainText;
         private SqlString key;
         private SqlString password;
-        
+
         [SetUp]
         public void Setup()
         {
@@ -19,17 +20,21 @@ namespace SecureLibrary.SQL.Tests
         }
         
         [Test]
+        [Category("Encryption")]
+        [Description("GenerateAESKey_Test")]
         public void GenerateAESKey_ShouldGenerateValidKey()
         {
             // Act
             var generatedKey = SqlCLRCrypting.GenerateAESKey();
             
             // Assert
-            Assert.That(!generatedKey.IsNull);
-            Assert.That(generatedKey.Value.Length, Is.EqualTo(44)); // Base64 length of 256-bit key
+            Assert.That(generatedKey, Is.Not.Null, "Generated key should not be null");
+            Assert.That(generatedKey.Value.Length, Is.EqualTo(44), "Key length should be 44 characters");
         }
         
         [Test]
+        [Category("Encryption")]
+        [Description("EncryptDecrypt_Test")]
         public void EncryptAES_ShouldEncryptAndDecryptSuccessfully()
         {
             // Act
@@ -37,20 +42,25 @@ namespace SecureLibrary.SQL.Tests
             var decrypted = SqlCLRCrypting.DecryptAES(encrypted[0], key, encrypted[1]);
             
             // Assert
-            Assert.That(decrypted.Value, Is.EqualTo(plainText.Value));
+            Assert.That(decrypted.Value, Is.EqualTo(plainText.Value), "Decrypted text should match original");
         }
         
         [Test]
+        [Category("Password")]
+        [Description("HashPassword_Test")]
         public void HashPassword_ShouldHashPasswordSuccessfully()
         {
             // Act
             var hashedPassword = SqlCLRCrypting.HashPassword(password);
             
             // Assert
-            Assert.That(!hashedPassword.IsNull);
+            Assert.That(!string.IsNullOrEmpty(hashedPassword.Value), "Hashed password should not be null");
+            Assert.That(hashedPassword.Value, Does.StartWith("$2"), "Hash should be in BCrypt format");
         }
         
         [Test]
+        [Category("Password")]
+        [Description("VerifyPassword_Test")]
         public void VerifyPassword_ShouldVerifyPasswordSuccessfully()
         {
             // Arrange
@@ -60,7 +70,7 @@ namespace SecureLibrary.SQL.Tests
             var isValid = SqlCLRCrypting.VerifyPassword(password, hashedPassword);
             
             // Assert
-            Assert.That(isValid.IsTrue);
+            Assert.That(isValid.IsTrue, Is.True, "Password verification should succeed");
         }
     }
 }
